05eab1990eaae6be6247aafee3fd84a1056637dc
Antonio Jose Crespo Arjona <antocres@gmail.com>
2019-06-19 11:50:45 +0200

    Spring Boot Starters. Starter thymeleaf e integración con WebJars (bootstrap)

---
+++ springbootthymeleaf/src/main/java/es/ajca/springbootthymeleaf/PersonaService.java
@@ -0,0 +1,24 @@
+package es.ajca.springbootthymeleaf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonaService {
+	
+	static List<Persona> lista = new ArrayList<>();
+	
+	static {
+		Persona p1 = new Persona("pepe","perez",30);
+		Persona p2 = new Persona("ana","sanchez",25);
+		lista.add(p1);
+		lista.add(p2);
+	}
+	
+	public List<Persona> buscarTodos() {
+		return lista;
+	}
+
+}
