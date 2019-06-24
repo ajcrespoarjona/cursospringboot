1f75672d3c4686180e9598b02e9e4489ac64d754
Antonio Jose Crespo Arjona <antocres@gmail.com>
2019-06-24 11:10:08 +0200

    Ejemplo Spring JPA Starter

---
+++ springbootjpa/src/main/java/es/ajca/springbootjpa/PersonaRepository.java
@@ -0,0 +1,9 @@
+package es.ajca.springbootjpa;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonaRepository extends CrudRepository<Persona, String> {
+
+	
+
+}
