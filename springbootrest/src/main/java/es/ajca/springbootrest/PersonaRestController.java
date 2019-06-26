d076efe860bb456433abb1a2cc4d03510ce1657e
Antonio Jose Crespo Arjona <antocres@gmail.com>
2019-06-26 12:21:25 +0200

    Spring Boot REST Continuación. Peticiones POST y DELETE (modelo servidor).

--- springbootrest/src/main/java/es/ajca/springbootrest/PersonaRestController.java
+++ springbootrest/src/main/java/es/ajca/springbootrest/PersonaRestController.java
@@ -1,7 +1,13 @@
 package es.ajca.springbootrest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +23,21 @@ public class PersonaRestController {
 
 		return repositorio.findAll();
 	}
+
+	@PostMapping(path = "/personas", consumes = "application/json")
+	public void insertar(@RequestBody Persona persona) {
+
+		repositorio.save(persona);
+	}
+
+	@DeleteMapping("/personas/{nombre}")
+	public void borrar(@PathVariable String nombre) {
+
+		Optional<Persona> persona = repositorio.findById(nombre);
+
+		if (persona.isPresent()) {
+			repositorio.delete(persona.get());
+		}
+	}
+
 }
