40f31779f37432023db4f44f24415e45f950b628
Antonio Jose Crespo Arjona <antocres@gmail.com>
2019-07-01 13:17:23 +0200

    Spring Boot REST. Single Page Applications. Parte II.2. Cliente Single Page Application con ajax y servidor de Parte II.1 (springbootservidorajax)

---
+++ springbootclienteajaxspa/src/main/java/es/ajca/springbootclienteajaxspa/SpringbootClienteAjaxSpaApplication.java
@@ -0,0 +1,13 @@
+package es.ajca.springbootclienteajaxspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootClienteAjaxSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootClienteAjaxSpaApplication.class, args);
+	}
+
+}
