b8a3090b1f93d54771d9e83116bba12cfa97f4f9
Antonio Jose Crespo Arjona <antocres@gmail.com>
2019-07-15 14:24:45 +0200

    Spring Boot Buenas Prácticas. I) Ficheros YAML. II) Desplegando en Tomcat con Spring Boot. III) Spring Boot Actuator

---
+++ springbootwar/src/main/java/es/ajca/springbootwar/SpringbootWarApplication.java
@@ -0,0 +1,9 @@
+package es.ajca.springbootwar;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootWarApplication extends SpringBootServletInitializer{
+	
+}
